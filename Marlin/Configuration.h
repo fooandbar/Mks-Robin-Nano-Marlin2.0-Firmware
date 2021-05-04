cf95075b6e937f8a5b8046fbc8bb74223c6b03e8
fooandbar <isobe@ttj.gr.jp>
2021-05-05 04:31:28 +0900

    remove FIX comments

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -122,7 +122,7 @@
  * :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000]
  */
 //#define BAUDRATE 250000
-#define BAUDRATE 115200 // FIX 2021/05/04
+#define BAUDRATE 115200 
 
 // Enable the Bluetooth serial interface on AT90USB devices
 //#define BLUETOOTH
@@ -459,18 +459,15 @@
 // Above this temperature the heater will be switched off.
 // This can protect components from overheating, but NOT from shorts and failures.
 // (Use MINTEMP for thermistor short/failure protection.)
-//#define HEATER_0_MAXTEMP 275
-#define HEATER_0_MAXTEMP 305  // FIX 2021/05/04
-//#define HEATER_1_MAXTEMP 275
-#define HEATER_1_MAXTEMP 275  // FIX 2021/05/04
+#define HEATER_0_MAXTEMP 305  
+#define HEATER_1_MAXTEMP 275  
 #define HEATER_2_MAXTEMP 275
 #define HEATER_3_MAXTEMP 275
 #define HEATER_4_MAXTEMP 275
 #define HEATER_5_MAXTEMP 275
 #define HEATER_6_MAXTEMP 275
 #define HEATER_7_MAXTEMP 275
-//#define BED_MAXTEMP      150
-#define BED_MAXTEMP      100  // FIX 2021/05/04
+#define BED_MAXTEMP      100  
 
 //===========================================================================
 //============================= PID Settings ================================
@@ -620,10 +617,10 @@
 // Specify here all the endstop connectors that are connected to any endstop or probe.
 // Almost all printers will be using one per axis. Probes will use one or more of the
 // extra connectors. Leave undefined any used for non-endstop and non-probe purposes.
-//#define USE_XMIN_PLUG // FIX 2021/05/04
+//#define USE_XMIN_PLUG 
 #define USE_YMIN_PLUG
 #define USE_ZMIN_PLUG
-#define USE_XMAX_PLUG // FIX 2021/05/04
+#define USE_XMAX_PLUG 
 //#define USE_YMAX_PLUG
 //#define USE_ZMAX_PLUG
 
@@ -654,13 +651,6 @@
 #endif
 
 // Mechanical endstop with COM to ground and NC to Signal uses "false" here (most common setup).
-//#define X_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-//#define Y_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-//#define Z_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-//#define X_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-//#define Y_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-//#define Z_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-// FIX 2021/05/04
 #define X_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
 #define Y_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
 #define Z_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
@@ -750,16 +740,14 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-//#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 133 }
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 520, 520, 8800, 800}  // FIX 2021/05/02
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 520, 520, 8800, 800}
 
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-//#define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 25 }
-#define DEFAULT_MAX_FEEDRATE          { 28, 28, 2.5, 30} // FIX 2021/05/02
+#define DEFAULT_MAX_FEEDRATE          { 28, 28, 2.5, 30}
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
@@ -772,8 +760,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-//#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 10000 }
-#define DEFAULT_MAX_ACCELERATION      { 11, 11, 2, 22 }  // FIX 2021/05/02
+#define DEFAULT_MAX_ACCELERATION      { 11, 11, 2, 22 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
@@ -788,10 +775,6 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-//#define DEFAULT_ACCELERATION          3000    // X, Y, Z and E acceleration for printing moves
-//#define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
-//#define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration for travel (non printing) moves
-// FIX 2021/05/02
 #define DEFAULT_ACCELERATION          22    // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  22    // E acceleration for retracts
 #define DEFAULT_TRAVEL_ACCELERATION   22    // X, Y, Z acceleration for travel (non printing) moves
@@ -857,7 +840,7 @@
  * The probe replaces the Z-MIN endstop and is used for Z homing.
  * (Automatically enables USE_PROBE_FOR_Z_HOMING.)
  */
-#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN  // FIX 2021/05/04
+#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN  
 
 // Force the use of the probe for Z-axis homing
 //#define USE_PROBE_FOR_Z_HOMING
@@ -916,7 +899,7 @@
  * The BLTouch probe uses a Hall effect sensor and emulates a servo.
  */
 //#define BLTOUCH
-#define BLTOUCH // FIX 2021/05/04
+#define BLTOUCH 
 
 /**
  * Touch-MI Probe by hotends.fr
@@ -1008,20 +991,17 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-//#define NOZZLE_TO_PROBE_OFFSET { -40, -14, -2 }
-#define NOZZLE_TO_PROBE_OFFSET { 0, -35, -1.3 } // FIX 2021/05/04
+#define NOZZLE_TO_PROBE_OFFSET { 0, -35, -1.3 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
 #define PROBING_MARGIN 30
 
 // X and Y axis travel speed (mm/min) between probes
-//#define XY_PROBE_SPEED (133*60)
-#define XY_PROBE_SPEED (4000) // FIX 2021/05/04
+#define XY_PROBE_SPEED (4000)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
-//#define Z_PROBE_SPEED_FAST (4*60)
-#define Z_PROBE_SPEED_FAST (600) // FIX 2021/05/04
+#define Z_PROBE_SPEED_FAST (600)
 
 // Feedrate (mm/min) for the "accurate" probe of each point
 #define Z_PROBE_SPEED_SLOW (Z_PROBE_SPEED_FAST / 2)
@@ -1079,9 +1059,6 @@
  * Example: `M851 Z-5` with a CLEARANCE of 4  =>  9mm from bed to nozzle.
  *     But: `M851 Z+1` with a CLEARANCE of 2  =>  2mm from bed to nozzle.
  */
-//#define Z_CLEARANCE_DEPLOY_PROBE   10 // Z Clearance for Deploy/Stow
-//#define Z_CLEARANCE_BETWEEN_PROBES  5 // Z Clearance between probe points
-// FIX 2021/05/04
 #define Z_CLEARANCE_DEPLOY_PROBE   10 // Z Clearance for Deploy/Stow
 #define Z_CLEARANCE_BETWEEN_PROBES  10 // Z Clearance between probe points
 #define Z_CLEARANCE_MULTI_PROBE     5 // Z Clearance between multiple probes
@@ -1149,15 +1126,13 @@
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR true
-//#define INVERT_Y_DIR true
-#define INVERT_Y_DIR false  // FIX 2021/05/02
+#define INVERT_Y_DIR false  
 #define INVERT_Z_DIR false
 
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-//#define INVERT_E0_DIR true
-#define INVERT_E0_DIR false // FIX 2021/05/02
+#define INVERT_E0_DIR false 
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
@@ -1186,10 +1161,8 @@
 // @section machine
 
 // The size of the print bed
-//#define X_BED_SIZE 220
-//#define Y_BED_SIZE 220
-#define X_BED_SIZE 120  // FIX 2021/05/02
-#define Y_BED_SIZE 120  // FIX 2021/05/02
+#define X_BED_SIZE 120
+#define Y_BED_SIZE 120
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1340,7 +1313,7 @@
  */
 //#define AUTO_BED_LEVELING_3POINT
 //#define AUTO_BED_LEVELING_LINEAR
-#define AUTO_BED_LEVELING_BILINEAR  // FIX 2021/05/04
+#define AUTO_BED_LEVELING_BILINEAR  
 //#define AUTO_BED_LEVELING_UBL
 //#define MESH_BED_LEVELING
 
@@ -1531,7 +1504,7 @@
 // - Move the Z probe (or nozzle) to a defined XY point before Z Homing.
 // - Prevent Z homing when the Z probe is outside bed area.
 //
-#define Z_SAFE_HOMING // FIX 2021/05/04
+#define Z_SAFE_HOMING 
 
 #if ENABLED(Z_SAFE_HOMING)
   #define Z_SAFE_HOMING_X_POINT X_CENTER  // X point for Z homing
@@ -1539,8 +1512,7 @@
 #endif
 
 // Homing speeds (mm/min)
-//#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (4*60) }
-#define HOMING_FEEDRATE_MM_M { (1800), (1800), (120) } // FIX 2021/05/04
+#define HOMING_FEEDRATE_MM_M { (1800), (1800), (120) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
@@ -2363,7 +2335,7 @@
 // 320x240, 2.4", FSMC Display From MKS
 // Normally used in MKS Robin Nano V1.2
 //
-#define MKS_ROBIN_TFT24 // FIX 2021/05/04
+#define MKS_ROBIN_TFT24 
 
 //
 // 320x240, 2.8", FSMC Display From MKS
@@ -2381,7 +2353,7 @@
 // 480x320, 3.5", FSMC Display From MKS
 // Normally used in MKS Robin Nano V1.2
 //
-//#define MKS_ROBIN_TFT35 // FIX 2021/05/04
+//#define MKS_ROBIN_TFT35 
 
 //
 // 480x272, 4.3", FSMC Display From MKS
