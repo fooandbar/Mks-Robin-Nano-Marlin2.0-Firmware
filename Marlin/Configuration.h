949f89a488274a6ee169e6f28d5b664031e7d53f
fooandbar <isobe@ttj.gr.jp>
2021-05-08 11:10:31 +0900

    modify feedrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -747,11 +747,11 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 120, 120, 10, 60}
+#define DEFAULT_MAX_FEEDRATE          { 120, 120, 10, 120}
 
 #define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
-  #define MAX_FEEDRATE_EDIT_VALUES    { 600, 600, 50, 100 } // ...or, set your own edit limits
+  #define MAX_FEEDRATE_EDIT_VALUES    { 600, 600, 50, 600 } // ...or, set your own edit limits
 #endif
 
 /**
